2568fa20007ab02116c5c7c45e46501a92e7f7f9
Jackson Silva <jackson.rodrigues9@hotmail.com>
2019-11-21 12:59:35 -0300

    Game completo

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,35 +1,30 @@
-module.exports.cadastro = function (application, req, res) {
-    res.render('cadastro', {
-        validacao: [],
-        dadosForm: {}
-    });
+module.exports.cadastro = function (application, req, res){
+	res.render('cadastro', {validacao: {}, dadosForm: {}});
 }
 
-module.exports.cadastrar = function (application, req, res) {
-    var dadosForm = req.body
+module.exports.cadastrar = function(application, req, res){
 
-    req.assert('nome', 'Nome não pode ser vazio!').notEmpty();
+	var dadosForm = req.body;
 
-    req.assert('usuario', 'Usuario não pode ser vazio!').notEmpty();
-    req.assert('senha', 'Senha não pode ser vazio!').notEmpty();
-    req.assert('casa', 'Casa não pode ser vazio!').notEmpty();
+	req.assert('nome', 'Nome não pode ser vazio').notEmpty();
+	req.assert('usuario', 'Usuário não pode ser vazio').notEmpty();
+	req.assert('senha', 'Senha não pode ser vazio').notEmpty();
+	req.assert('casa', 'Casa não pode ser vazio').notEmpty();
 
-    var erros = req.validationErrors();
+	var erros = req.validationErrors();
 
-    if (erros) {
-        res.render('cadastro', {
-            validacao: erros,
-            dadosForm
-        })
-        return;
-    }
-    var connection = application.config.dbConnection;
-    const Usuario = new application.app.models.Usuarios(connection);
+	if(erros){
+		res.render('cadastro', {validacao: erros, dadosForm: dadosForm});
+		return;
+	}
 
-    Usuario.inserirUsuario(dadosForm);
+	var connection = application.config.dbConnection;
+	var UsuariosDAO = new application.app.models.UsuariosDAO(connection);
+	var JogoDAO = new application.app.models.JogoDAO(connection);
 
-    res.render('cadastro', {
-        validacao: [],
-        dadosForm: {}
-    });
+	UsuariosDAO.inserirUsuario(dadosForm);
+	JogoDAO.gerarParametros(dadosForm.usuario);
+	//geração dos parametros
+
+	res.send('podemos cadastrar')
 }
