f8591e777abe57c10827d9c477dbe47bb86587aa
Jackson Silva <jackson.rodrigues9@hotmail.com>
2019-11-21 11:32:49 -0300

    Connectando com o mongodb

---
+++ config/dbConnection.js
@@ -0,0 +1,17 @@
+var mongo = require('mongodb');
+
+var connect = () => {
+    var db = new mongo.Db(
+        'got',
+        new mongo.Server(
+            'localhost',
+            27017, {}
+        ), {}
+    )
+
+    return db;
+}
+
+module.exports = () => {
+    return connect;
+}
